3ca9b1771eb6585346e3cf72c5e7b91bb2ce1f61
Sambor Witek <63224@student.ue.poznan.pl>
2023-04-16 17:50:49 +0200

    Dodano trapez

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -36,4 +36,14 @@ obwodprostokata = 2 * a + 2 * b
 print("Pole prostokata wynosi " + str(poleprostokata) + ", a obwod wynosi " + str(obwodprostokata))
 
 
+#trapez
 
+a = 10
+b = 5
+c = 3
+d = 7
+h = 6
+obwodtrapezu = a+b+c+d
+poletrapezu = (a+b)*h/2
+
+print("Pole trapezu wynosi " + str(poletrapezu) + ", a obwod wynosi " + str(obwodtrapezu))
